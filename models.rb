f6436ca8e6a8328e926fab38162fe8f6ca2fab2e
Kamil Mosciszko <kamil.mazurek319@gmail.com>
2025-08-01 16:44:15 +0200

    Fix MCP provider config handling

--- models.rb
+++ models.rb
@@ -82,8 +82,19 @@ class SSEProvider < Provider
 end
 
 class MCPProvider < Provider
-  def initialize(name:, **attrs)
-    super(name: name, provider_type: 'mcp', **attrs)
+  def initialize(name:, config:, **attrs)
+    super(name: name, provider_type: 'mcp', config: self.class.deep_ostruct(config), **attrs)
+  end
+
+  def self.deep_ostruct(obj)
+    case obj
+    when Hash
+      OpenStruct.new(obj.transform_values { |v| deep_ostruct(v) })
+    when Array
+      obj.map { |v| deep_ostruct(v) }
+    else
+      obj
+    end
   end
 end
 
